cbad753ecdf91aa77f147856db07acc411d8ef13
pawanpreet kaur <pawanpreetkaur@pawanpreets-mac.local>
2017-01-17 22:18:41 -0500

    Initial Commit

---
+++ Calculator app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator app
+//
+//  Created by pawanpreet kaur on 2017-01-17.
+//  Copyright © 2017 Pawanpreet kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
